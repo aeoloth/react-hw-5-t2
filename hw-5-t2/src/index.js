e9c592b51d7f87c319fe813d2d5d7dcccfb6c98b
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-08 20:25:59 +0300

    2

--- hw-5-t2/src/index.js
+++ hw-5-t2/src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import "./App.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Season = ({ value }) => {
+//   return (
+//     <div className="season">
+//       <img className="season__img" src="/img/winter.jpg" alt="" />
+//       <div className="season__name">Зима</div>
+//     </div>
+//   );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+if (value === 1) {
+  return (
+    <div className="season">
+      <img className="season__img" src="/img/winter.jpg" alt="winter" />
+      <div className="season__name">Зима</div>
+    </div>
+  );
+} else if (value === 2) {
+  return (
+    <div className="season">
+      <img className="season__img" src="/img/spring.jpg" alt="spring" />
+      <div className="season__name">Весна</div>
+    </div>
+    );
+} else if (value === 3) {
+  return (
+    <div className="season">
+      <img className="season__img" src="/img/summer.jpg" alt="summer" />
+      <div className="season__name">Лето</div>
+    </div>
+    );
+} else {
+  return (
+    <div className="season">
+      <img className="season__img" src="/img/autumn.jpg" alt="autumn" />
+      <div className="season__name">Осень</div>
+    </div>
+    );
+};
+
+
+
+
+};
+
+Season.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+ReactDOM.render(<Season value={1} />, document.getElementById("root"))
